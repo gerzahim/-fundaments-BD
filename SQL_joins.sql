d659c7c7fb874ed23bdafc0a68845b239a0830f0
Gerzahim Salas <rasce88@gmail.com>
2021-10-16 15:34:59 -0400

    Adding INNER JOIN and OUTER JOIN
    Adding INNER JOIN and OUTER JOIN

--- SQL_joins.sql
+++ SQL_joins.sql
@@ -32,6 +32,7 @@ WHERE posts.usuario_id IS NULL
 
 -- LEFT JOIN
 -- traer todos los usuarios que obligatoriamente (TENGAN) posts 
+
 SELECT * 
 FROM usuarios
 LEFT JOIN posts
@@ -45,6 +46,58 @@ WHERE posts.usuario_id IS NOT NULL
 |      4 | Ed       |     57 | Escndalo con el boxeador del momento     | TIENE POST
 |      4 | Ed       |     61 | Escndalo en el mundo de la moda          | TIENE POST
 +--------+----------+--------+------------------------------------------+
+
+
+
+-- INNER JOIN
+-- traer todos los usuarios que obligatoriamente (TENGAN) posts y 
+-- y todos los posts obligatoriamente (TENGAN) usuarios
+SELECT * 
+FROM usuarios
+INNER JOIN posts
+ ON posts.usuario_id = usuarios.id
++--------+----------+--------+------------------------------------------+
+| userID | nickname | postID | post_titulo                              |
++--------+----------+--------+------------------------------------------+
+|      4 | Ed       |     46 | Los mejores vestidos en la alfombra roja | TIENE POST y TIENE USUARIO
+|      4 | Ed       |     47 | Los paparatzi captan escndalo en cmara   | TIENE POST y TIENE USUARIO
+|      4 | Ed       |     57 | Escndalo con el boxeador del momento     | TIENE POST y TIENE USUARIO
+|      4 | Ed       |     61 | Escndalo en el mundo de la moda          | TIENE POST y TIENE USUARIO
++--------+----------+--------+------------------------------------------+
+
+
+
+-- OUTER JOIN
+-- traer todos los usuarios y posts que (TENGAN o NO TENGAN) relacion entre ellos 
+SELECT * 
+FROM usuarios
+FULL OUTER JOIN posts
+ ON posts.usuario_id = usuarios.id
++--------+----------+--------+------------------------------------------+
+| userID | nickname | postID | post_titulo                              |
++--------+----------+--------+------------------------------------------+
+|      5 | Oso Prez |   NULL | NULL                                     | NO TIENE POST
+|      4 | Ed       |     47 | Los paparatzi captan escndalo en cmara   | TIENE POST
+|      4 | Ed       |     57 | Escndalo con el boxeador del momento     | TIENE POST
+|   NULL | NULL     |     34 | mariposa de la moda                      | NO TIENE USUARIO
++--------+----------+--------+------------------------------------------+
+
+
+-- OUTER JOIN
+-- traer todos los usuarios y posts que (NO TENGAN) relacion entre ellos 
+SELECT * 
+FROM usuarios
+FULL OUTER JOIN posts
+ ON posts.usuario_id = usuarios.id
+WHERE posts.usuario_id IS NULL 
+OR usuarios.id IS NULL 
++--------+----------+--------+------------------------------------------+
+| userID | nickname | postID | post_titulo                              |
++--------+----------+--------+------------------------------------------+
+|      5 | Oso Prez |   NULL | NULL                                     | NO TIENE POST
+|   NULL | NULL     |     34 | mariposa de la moda                      | NO TIENE USUARIO
++--------+----------+--------+------------------------------------------+
+
 
 
 
